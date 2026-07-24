fba8caaa6060d41f2f7e15d09cf790de35d1d190
tanishk <tanishk006@gmail.com>
2026-07-25 02:40:26 +0530

    Code : Even or Odd with user input using for loop with specified output

---
+++ Assignments/W_1_25/EOp3.java
@@ -0,0 +1,39 @@
+package W_1_25;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class EOp3
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+       int[] evens = new int[5];
+       int[] odds = new int[5];
+
+       int input;
+
+       int eveni =0;
+       int oddi =0;
+
+       for(int i = 0 ; i < 5 ;i++)
+       {
+           input = sc.nextInt();
+
+           if(input%2==0)
+           {
+               evens[eveni] = input;
+               eveni++;
+           }
+           else{
+               odds[oddi] = input;
+               oddi++;
+           }
+       }
+        System.out.println("Even "+ Arrays.toString(evens));
+        System.out.println("Odd "+ Arrays.toString(odds));
+
+    }
+
+}
