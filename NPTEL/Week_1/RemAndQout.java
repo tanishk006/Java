4eec93771cb3b0de296b45e92b2dc0a0dec59723
tanishk <tanishk006@gmail.com>
2026-07-24 23:55:55 +0530

    feat : RemAndQout

--- NPTEL/Week_1/RemAndQout.java
+++ NPTEL/Week_1/RemAndQout.java
@@ -1,2 +1,20 @@
 // Find Remainder and Qoutient using user input
+import java.util.Scanner;
+
+Public class RanQ{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int qoutient = x / y;
+        int remainder = x % y;
+
+        System.out.println("The Qoutient is =" + qoutient);
+        System.out.println("The Remainder is =" + remainder);
+
+
+     sc.close();
+    }
+}
 
