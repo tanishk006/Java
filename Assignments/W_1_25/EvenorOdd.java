3131cff906b11410cb61c9368809e172a5646270
tanishk <tanishk006@gmail.com>
2026-07-25 01:15:12 +0530

    Code : Even or Odd with user input

---
+++ Assignments/W_1_25/EvenorOdd.java
@@ -0,0 +1,25 @@
+package W_1_25;
+
+import java.util.Scanner;
+
+public class EvenorOdd
+{
+    public static void main(String[] args){
+
+                Scanner in = new Scanner(System.in);
+                int number = in.nextInt();
+
+                System.out.print("Enter the number");
+
+                if (number % 2 == 0) {
+                    System.out.println(number + " is Even");
+                }
+                else
+                {
+                    System.out.println(number + " is Odd");
+                }
+
+                in.close();
+
+    }
+}
