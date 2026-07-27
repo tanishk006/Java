e9747e35462166d349231f4550731851005c3a38
tanishk <tanishk006@gmail.com>
2026-07-27 23:18:14 +0530

    Code : NPTEL Week 2 , Assignment 3

---
+++ NPTEL/Week_2/MaxElementArr.java
@@ -0,0 +1,33 @@
+package Week_2;
+
+import java.util.Scanner;
+
+
+public class MaxElementArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        int max = arr[0];
+
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+
+        System.out.println("Maximum is: " + max);
+        sc.close();
+    }
+}
