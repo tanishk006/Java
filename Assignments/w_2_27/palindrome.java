30c00725ad577b586a00d1829f5506a32daadd0b
Tanishk Rawat <tanishkr006@gmail.com>
2026-07-28 05:27:43 +0000

    Code :  Lab Assignment Palindrome

---
+++ Assignments/w_2_27/palindrome.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class palindrome
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = sc.nextLine();
+        
+        String rev ="";
+
+
+       for(int i = word.length() - 1 ; i >= 0 ; i--)
+       {
+         rev = rev + word.charAt(i);
+       }
+         if (word.equalsIgnoreCase(rev)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+
+        sc.close();
+
+
+    }
+}
