f082f587b52fa35c87da0d4d8d8b7ffaad50b17c
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-04 04:03:12 +0000

    Code : NPTEL Week 3 , Assignment 1

---
+++ NPTEL/Week_3/factorial.java
@@ -0,0 +1,24 @@
+package NPTEL.Week_3;
+
+// factorial of a number 
+
+import java.util.Scanner;
+
+public class factorial {
+   public static void main(String[] args)
+   {
+    int fact = 1;
+    Scanner sc = new Scanner (System.in);
+    System.out.print("Enter a number :");
+    int n = sc.nextInt();
+
+    for(int i = 1; i <= n ;i++)
+    {
+        fact = fact*i;
+    }
+
+    System.out.println("Factorial is :" + fact);
+
+    sc.close();
+   }    
+}
