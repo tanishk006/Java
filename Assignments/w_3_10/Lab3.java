6c2365255350037ff9356ace32c8e120c72ecbdf
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-25 03:49:07 +0000

    Code : Assignment , circle

--- Assignments/w_3_10/Lab3.java
+++ Assignments/w_3_10/Lab3.java
@@ -1,35 +1,41 @@
-// Write a program that creates a class circle with instances variables for the centre
-//  and the radius . initlaize and display it vairbales.
-
 package w_3_10;
 
+import java.util.Scanner;
 
+public class Lab3 {
 
-public class Lab3{
-    
-    public static class Circle{
-       int  center;
-        double  radius;
-        
+    public static class Circle {
+        int centerX;
+        int centerY;
+        double radius;
 
-        public Circle(int center, double radius)
-        {
-             this.center = center;
-             this.radius = radius;
+        public Circle(int centerX, int centerY, double radius) {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.radius = radius;
         }
 
-           public void displayDetails() {
-            System.out.println("Circle Center: " + this.center);
+        public void displayDetails() {
+            System.out.println("Circle Center: (" + this.centerX + ", " + this.centerY + ")");
             System.out.println("Circle Radius: " + this.radius);
         }
     }
 
-    public static void main(String[] args)
-    { 
-        Circle myCircle = new Circle(6 , 3);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter center X coordinate: ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter center Y coordinate: ");
+        int y = sc.nextInt();
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        Circle myCircle = new Circle(x, y, radius);
         myCircle.displayDetails();
 
-
+        sc.close();
     }
 }
