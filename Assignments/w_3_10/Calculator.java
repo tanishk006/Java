b54c46bd37cf9fcfaa6b2af11a10538f319a42be
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-31 16:32:11 +0000

    Calculator using switch

---
+++ Assignments/w_3_10/Calculator.java
@@ -0,0 +1,47 @@
+package w_3_10;
+
+import java.util.Scanner;
+
+class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        switch (operator) {
+
+            case '+':
+                System.out.println("Result = " + (a + b));
+                break;
+
+            case '-':
+                System.out.println("Result = " + (a - b));
+                break;
+
+            case '*':
+                System.out.println("Result = " + (a * b));
+                break;
+
+            case '/':
+                if (b != 0)
+                    System.out.println("Result = " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+        }
+
+        sc.close();
+    }
+}
