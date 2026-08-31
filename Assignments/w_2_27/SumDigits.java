ade5a30b4b7693b8b903658a89b6389487af878d
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-31 16:33:57 +0000

    Sum of digits

---
+++ Assignments/w_2_27/SumDigits.java
@@ -0,0 +1,25 @@
+package w_2_27;
+
+import java.util.Scanner;
+
+class SumDigits {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
