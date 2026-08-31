42da9cea760cac13622ea86be80d68ce806f813a
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-31 16:12:41 +0000

    Perfect Number or not

---
+++ Assignments/W_4_31/PerfectNum.java
@@ -0,0 +1,28 @@
+package W_4_31;
+
+import java.util.Scanner;
+
+class PerfectNum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0)
+                sum += i;
+        }
+
+        if (sum == n)
+            System.out.println("Perfect Number");
+        else
+            System.out.println("Not a Perfect Number");
+
+        sc.close();
+    }
+}
