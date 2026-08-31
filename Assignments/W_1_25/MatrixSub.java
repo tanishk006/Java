63bb3dffeb2c330fe4adc2d04da5a0e20e926c35
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-31 16:51:14 +0000

    Matrix Subtraction

---
+++ Assignments/W_1_25/MatrixSub.java
@@ -0,0 +1,45 @@
+package W_1_25;
+
+import java.util.Scanner;
+
+class MatrixSub {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows: ");
+        int r = sc.nextInt();
+
+        System.out.print("Enter columns: ");
+        int c = sc.nextInt();
+
+        int a[][] = new int[r][c];
+        int b[][] = new int[r][c];
+
+        System.out.println("Enter first matrix:");
+
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                a[i][j] = sc.nextInt();
+
+        System.out.println("Enter second matrix:");
+
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                b[i][j] = sc.nextInt();
+
+        System.out.println("Result:");
+
+        for (int i = 0; i < r; i++) {
+
+            for (int j = 0; j < c; j++) {
+                System.out.print((a[i][j] - b[i][j]) + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
