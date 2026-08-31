e4d619d93aff41b7ba65c95c31cd45fc449d3859
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-31 16:09:16 +0000

    Frequency of a Character

---
+++ Assignments/W_4_31/CharFrequency.java
@@ -0,0 +1,28 @@
+package W_4_31;
+
+import java.util.Scanner;
+
+class CharFrequency {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        int freq[] = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) {
+                System.out.println((char) i + " = " + freq[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
