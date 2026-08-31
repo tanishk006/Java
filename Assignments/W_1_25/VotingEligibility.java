b5b6f6a4dc9c77568f9555fd8708794911b862b8
Tanishk Rawat <tanishkr006@gmail.com>
2026-08-31 16:58:31 +0000

    Check for Voting Eligibility

---
+++ Assignments/W_1_25/VotingEligibility.java
@@ -0,0 +1,21 @@
+package W_1_25;
+
+import java.util.Scanner;
+
+class VotingEligibility {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        if (age >= 18)
+            System.out.println("Eligible to vote");
+        else
+            System.out.println("Not eligible to vote");
+
+        sc.close();
+    }
+}
