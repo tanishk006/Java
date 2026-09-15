8346b2b82e19f85582fc4390d4d7b73af4e8c053
Tanishk Rawat <tanishkr006@gmail.com>
2026-09-15 04:31:31 +0000

    Code : Constructor Overloading

---
+++ Assignments/W_5_15/ConstructorOverloadingDemo.java
@@ -0,0 +1,36 @@
+package W_5_15;
+
+class Student {
+    String name;
+    int age;
+
+    Student() {
+        name = "Unknown";
+        age = 0;
+    }
+
+    Student(String n) {
+        name = n;
+        age = 0;
+    }
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+public class ConstructorOverloadingDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Tanishk");
+        Student s3 = new Student("Riya", 20);
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
