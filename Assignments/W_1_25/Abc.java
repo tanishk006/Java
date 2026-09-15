7535ea2f9b7fea5e3f882003580a5ff5f38549fa
Tanishk Rawat <tanishkr006@gmail.com>
2026-09-15 04:31:58 +0000

    Fix : Code fixes

---
+++ Assignments/W_1_25/Abc.java
@@ -0,0 +1,40 @@
+package w_1_25;
+
+import java.util.Scanner;
+
+class AbC 
+{
+    int result;
+
+    public Abc(int a, int b) 
+{
+        this.result = a * b;
+    }
+
+    public Abc(int a, int b, int c) 
+{
+        this.result = a * b * c;
+    }
+
+    public void showResult() 
+{
+        System.out.println("The product is: " + result);
+    }
+
+    public static void main(String[] args) 
+{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        Abc sum1 = new Abc(num1, num2);
+        sum1.showResult();
+
+        System.out.print("\nEnter first number: ");
+        int x = sc.nextInt();
+    }
+}
--- Assignments/W_1_25/EvenorOdd.java
+++ Assignments/W_1_25/EvenorOdd.java
@@ -6,11 +6,11 @@ public class EvenorOdd
 {
     public static void main(String[] args)
     {
-
+              System.out.println("Enter the number :");
                 Scanner in = new Scanner(System.in);
                 int number = in.nextInt();
 
-                System.out.print("Enter the number");
+               
 
                 if (number % 2 == 0) {
                     System.out.println(number + " is Even");
